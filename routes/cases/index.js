489ea0a7a7a11a37eaf598f279ce839828213b86
Kaushal Kapasi <kaushal.kapasi@td.com>
2016-10-19 11:18:48 -0400

    all /case requests respond with json format with message as the key for valid responses and error as the key for failures

--- routes/cases/index.js
+++ routes/cases/index.js
@@ -10,60 +10,66 @@ module.exports = function (router, db) {
 
                 caseDetails.save()
                     .then(function (caseDetails) {
-                        res.status(201).send(caseDetails);
+                        res.status(201).send({
+                            message: caseDetails
+                        });
                     })
                     .catch(function (err) {
-                        res.status(400).send('Failed to create case.');
+                        res.status(400).send({ error: 'Failed to create case.' });
                     });
             });
 
-            router.get(resource + "/all", function (req, res) {
+            router.get(resource + "s", function (req, res) {
                 db.models.Cases.find({}).exec()
                     .then(function (allCases) {
-                        res.send(allCases);
+                        res.send({
+                            message: allCases
+                        });
                     })
                     .catch(function (err) {
-                        res.status(400).send('Failed to get cases.');
+                        res.status(400).send({ error: 'Failed to get cases.' });
                     });
             });
 
             router.get(resource + "/:caseNumber", function (req, res) {
                 db.models.Cases.findByCaseNumber(req.params.caseNumber)
                     .then(function (caseDetails) {
-                        res.send(caseDetails);
+                        res.send({
+                            message: caseDetails
+                        });
                     })
                     .catch(function (err) {
-                        res.status(400).send('Failed to get case');
+                        res.status(400).send({ error: 'Failed to get case.' });
                     });
             });
 
             router.post(resource + "/:caseNumber", function (req, res) {
                 db.models.Cases.update({ number: req.params.caseNumber }, req.body, { overwrite: true }).exec()
                     .then(function (updatedCaseDetails) {
-                        res.status(200).send('Case ' + req.params.caseNumber + ' details updated.');
+                        res.status(200).send({ message: 'Case ' + req.params.caseNumber + ' details updated.' });
                     })
                     .catch(function (err) {
-                        res.status(500).send('Failed to update Case ' + req.params.caseNumber + ' details.');
+                        res.status(500).send({ error: 'Failed to update Case ' + req.params.caseNumber + ' details.' });
                     });
             });
 
             router.post(resource + "/:caseNumber" + "/checkin", function (req, res) {
                 db.models.Cases.update({ number: req.params.caseNumber }, { $set: { location: 'home' } }).exec()
                     .then(function (updatedCaseDetails) {
-                        res.status(200).send('Case ' + req.params.caseNumber + ' details updated.');
+                        res.status(200).send({ message: 'Case ' + req.params.caseNumber + ' check in confirmed.' });
                     })
                     .catch(function (err) {
-                        res.status(500).send('Failed to check in case.');
+                        res.status(500).send({ error: 'Failed to check in case.' });
                     });
             });
 
             router.post(resource + "/:caseNumber" + "/checkout", function (req, res) {
                 db.models.Cases.update({ number: req.params.caseNumber }, { $set: { location: req.body.event } }).exec()
                     .then(function (updatedCaseDetails) {
-                        res.status(200).send('Case ' + req.params.caseNumber + ' details updated.');
+                        res.status(200).send({ message: 'Case ' + req.params.caseNumber + ' check out confirmed.' });
                     })
                     .catch(function (err) {
-                        res.status(500).send('Failed to check out case.');
+                        res.status(500).send({ error: 'Failed to check out case.' });
                     });
             });
 
@@ -73,10 +79,12 @@ module.exports = function (router, db) {
                         return db.models.Products.find({ caseId: caseDetails._id }).exec();
                     })
                     .then(function (productsInCase) {
-                        res.send(productsInCase);
+                        res.send({
+                            message: productsInCase
+                        });
                     })
                     .catch(function (err) {
-                        res.status(500).send('Failed to get products in case ' + req.params.caseNumber + '.');
+                        res.status(500).send({ error: 'Failed to get products in case ' + req.params.caseNumber + '.' });
                     })
             });
         }
