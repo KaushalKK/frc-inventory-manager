dd1ae6803a65e9317f4a60e749f74d76fe305f32
Kaushal Kapasi <kaushal.kapasi@td.com>
2017-01-23 16:26:10 -0500

    rename new to create for use with products and cases

---
+++ public/js/create-dir.js
@@ -0,0 +1,82 @@
+"use strict";
+
+angular.module('inventorySystem').directive('create', ['inventoryService', 'toastr', function (inventoryService, toastr) {
+	return {
+		restrict: 'AE',
+		templateUrl: '../templates/create.html',
+		replace: true,
+		link: function (scope) {
+			scope.typeDropdown = [
+				{ name: "Case", value: "case" },
+				{ name: "Produt", value: "product" },
+				{ name: "Tote / Bin", value: "tote" }
+			];
+			scope.statusOptions = ["Storage", "At Event"];
+
+			function init() {
+				scope.assetTag = "";
+				scope.description = "";
+				scope.type = scope.typeDropdown[0];
+				scope.status = scope.statusOptions[0];
+
+				scope.caseNumber = "";
+				scope.itemName = "";
+				scope.itemPrice = "";
+				scope.itemQuantity = "";
+				scope.itemInCase = "";
+				scope.itemCountInCase = "";
+			}
+
+			scope.createItem = function () {
+				var itemToSave = {
+					location: scope.status,
+					barcode: scope.assetTag,
+					category: scope.type.value,
+					description: scope.description
+				};
+
+				if (scope.type.value === 'case' || scope.type.value === 'tote') {
+					itemToSave.number = scope.caseNumber;
+
+					inventoryService.createCase(itemToSave)
+						.then(function () {
+							toastr.success('Case saved.');
+						})
+						.catch(function () {
+							toastr.error('Failed to save case.');
+						});
+				} else {
+					itemToSave.name = scope.itemName;
+					itemToSave.price = scope.itemPrice;
+					itemToSave.quantity = scope.itemQuantity;
+
+					itemToSave.caseId = scope.itemInCase;
+					itemToSave.caseQuantity = scope.itemCountInCase;
+
+					inventoryService.createProduct(itemToSave)
+						.then(function () {
+							toastr.success('Case saved.');
+						})
+						.catch(function () {
+							toastr.error('Failed to save case.');
+						});
+				}
+			}
+
+			scope.cancel = function () {
+				init();
+			}
+
+			scope.toggle = function () {
+				if (scope.status === scope.statusOptions[0]) {
+					scope.status = scope.statusOptions[1];
+				} else {
+					scope.status = scope.statusOptions[0];
+
+				}
+			}
+
+			init();
+		}
+	};
+}]);
